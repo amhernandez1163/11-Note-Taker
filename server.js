cbbb2fdfa93ea46298d4882e43654ee3043380fd
Amanda Dell <amhernandez1163@gmail.com>
2022-04-23 14:30:15 -0500

    initial file set up and api get to link to starter code

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const notes = require("./db/db.json");
 const path = require("path");
 
 const PORT = process.env.PORT || 3001;
@@ -10,9 +11,18 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // API routes
-
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("/api/notes", (req, res) => {
+  console.log("reach");
+  console.log(notes);
+  res.json(notes);
+});
+
+app.post("/api/notes", (req, res) => {
+  console.log(req.body);
 });
 
 app.get("*", (req, res) => {
