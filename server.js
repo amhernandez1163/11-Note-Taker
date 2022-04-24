c1563571efca15c9ea0daed193b443a49cb7069a
Amanda Dell <amhernandez1163@gmail.com>
2022-04-23 21:29:37 -0500

    updated app.get and app.post to save note

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const fs = require("fs");
 const notes = require("./db/db.json");
 const path = require("path");
+// const { saveNote } = require("./db/noteFunctions");
+var uuidv1 = require("uuidv1");
+// console.log(uuidv1())
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -16,13 +20,23 @@ app.get("/notes", (req, res) => {
 });
 
 app.get("/api/notes", (req, res) => {
-  console.log("reach");
-  console.log(notes);
-  res.json(notes);
+  const savedNote = fs.readFileSync(path.join(__dirname, "./db/db.json"));
+  const parseNote = JSON.parse(savedNote);
+  res.json(parseNote);
 });
 
 app.post("/api/notes", (req, res) => {
+  const savedNote = fs.readFileSync(path.join(__dirname, "./db/db.json"));
+  const parseNote = JSON.parse(savedNote);
   console.log(req.body);
+  req.body.id = uuidv1;
+  parseNote.push(req.body);
+
+  fs.writeFileSync(
+    path.join(__dirname, "./db/db.json"),
+    JSON.stringify(parseNote)
+  );
+  res.json(notes);
 });
 
 app.get("*", (req, res) => {
